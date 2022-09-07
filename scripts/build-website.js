c29f3a0cfa53897024679430b81c5b28200718de
Khadeeejah <wuraola81@gmail.com>
2022-09-07 22:38:37 +0400

    mutate the js file

--- scripts/build-website.js
+++ scripts/build-website.js
@@ -14,10 +14,19 @@ async function prepareArtifacts() {
   await Promise.all([
     fs.copyFile(path.join(rootPath, 'index.html'), path.join(publicPath, 'index.html')),
     fs.copyFile(path.join(rootPath, 'index.css'), path.join(publicPath, 'index.css')),
-    fs.copyFile(path.join(rootPath, 'index.js'), path.join(publicPath, 'index.js')),
     fs.copyFile(path.join(rootPath, 'images', 'icon.svg'), path.join(imagesPath, 'icon.svg')),
     fs.copyFile(path.join(rootPath, 'dist', 'bundle.js'), path.join(distPath, 'bundle.js')),
     fs.copyFile(path.join(rootPath, 'snap.manifest.json'), path.join(publicPath, 'snap.manifest.json')),
+    fs.writeFile(
+      path.join(publicPath, 'index.js'),
+      (
+        await fs.readFile(path.join(rootPath, 'index.js'), 'utf8')
+      ).replace(
+        // eslint-disable-next-line no-template-curly-in-string
+        'const snapId = `local:${window.location.href}`;',
+        'const snapId = `npm:@khadeeejah/tokensnap`;',
+      ),
+    ),
   ]);
 }
 
