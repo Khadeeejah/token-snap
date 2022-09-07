8fd24212c2c9a85bd8bf118109d4e9adeb96fa59
Khadeeejah <wuraola81@gmail.com>
2022-09-07 23:59:37 +0400

    remove snapish files from website

--- scripts/build-website.js
+++ scripts/build-website.js
@@ -6,17 +6,13 @@ async function prepareArtifacts() {
   const rootPath = path.resolve(__dirname, '..');
   const publicPath = path.join(rootPath, 'public');
   const imagesPath = path.join(publicPath, 'images');
-  const distPath = path.join(publicPath, 'dist');
 
-  await mkdirp(distPath);
   await mkdirp(imagesPath);
 
   await Promise.all([
     fs.copyFile(path.join(rootPath, 'index.html'), path.join(publicPath, 'index.html')),
     fs.copyFile(path.join(rootPath, 'index.css'), path.join(publicPath, 'index.css')),
     fs.copyFile(path.join(rootPath, 'images', 'icon.svg'), path.join(imagesPath, 'icon.svg')),
-    fs.copyFile(path.join(rootPath, 'dist', 'bundle.js'), path.join(distPath, 'bundle.js')),
-    fs.copyFile(path.join(rootPath, 'snap.manifest.json'), path.join(publicPath, 'snap.manifest.json')),
     fs.writeFile(
       path.join(publicPath, 'index.js'),
       (
