44b8841b6103bc41719bcc3332cde5a30dce9b1a
Khadeeejah <wuraola81@gmail.com>
2022-09-07 16:57:49 +0400

    copy over artifacts for website build

--- scripts/build-website.js
+++ scripts/build-website.js
@@ -11,14 +11,17 @@ async function main() {
 
   await mkdirp(imagesPath);
   await fs.copyFile(path.join(rootPath, 'images/icon.svg'), path.join(imagesPath, 'icon.svg'));
+  await fs.copyFile(path.join(rootPath, 'index.html'), path.join(publicPath, 'index.html'));
+  await fs.copyFile(path.join(rootPath, 'bundle.js'), path.join(publicPath, 'bundle.js'));
+  await fs.copyFile(path.join(rootPath, 'index.js'), path.join(publicPath, 'index.js'));
 
-  const htmlContents = await fs.readFile(path.join(rootPath, 'index.html'), 'utf8');
-  await fs.writeFile(
-    path.join(publicPath, 'index.html'),
-    htmlContents.replace(
-      // eslint-disable-next-line no-template-curly-in-string
-      'const snapId = `local:${window.location.href}`;',
-      'const snapId = `npm:@metamask/template-snap`;',
-    ),
-  );
+  // const htmlContents = await fs.readFile(path.join(rootPath, 'index.html'), 'utf8');
+  // await fs.writeFile(
+  //   path.join(publicPath, 'index.html'),
+  //   htmlContents.replace(
+  //     // eslint-disable-next-line no-template-curly-in-string
+  //     'const snapId = `local:${window.location.href}`;',
+  //     'const snapId = `npm:@metamask/template-snap`;',
+  //   ),
+  // );
 }
