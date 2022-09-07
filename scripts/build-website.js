8e230b691925c538502b40bb700b145169e43c06
Khadeeejah <wuraola81@gmail.com>
2022-09-07 21:36:54 +0400

    revamp website build process

--- scripts/build-website.js
+++ scripts/build-website.js
@@ -1,24 +1,24 @@
-const { promises: fs } = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 const mkdirp = require('mkdirp');
 
-main();
+async function prepareArtifacts() {
+  const rootPath = path.resolve(__dirname, '..');
+  const publicPath = path.join(rootPath, 'public');
+  const imagesPath = path.join(publicPath, 'images');
+  const distPath = path.join(publicPath, 'dist');
 
-async function main() {
-  const rootPath = path.resolve(__dirname, '../');
-  const publicPath = path.join(rootPath, 'public/');
-  const imagesPath = path.join(publicPath, 'images/');
-
+  await mkdirp(distPath);
   await mkdirp(imagesPath);
-  await fs.copyFile(path.join(rootPath, 'images/icon.svg'), path.join(imagesPath, 'icon.svg'));
 
-  const htmlContents = await fs.readFile(path.join(rootPath, 'index.html'), 'utf8');
-  await fs.writeFile(
-    path.join(publicPath, 'index.html'),
-    htmlContents.replace(
-      // eslint-disable-next-line no-template-curly-in-string
-      'const snapId = `local:${window.location.href}`;',
-      'const snapId = `npm:@metamask/template-snap`;',
-    ),
-  );
+  await Promise.all([
+    fs.copyFile(path.join(rootPath, 'index.html'), path.join(publicPath, 'index.html')),
+    fs.copyFile(path.join(rootPath, 'index.css'), path.join(publicPath, 'index.css')),
+    fs.copyFile(path.join(rootPath, 'index.js'), path.join(publicPath, 'index.js')),
+    fs.copyFile(path.join(rootPath, 'images', 'icon.svg'), path.join(imagesPath, 'icon.svg')),
+    fs.copyFile(path.join(rootPath, 'dist', 'bundle.js'), path.join(distPath, 'bundle.js')),
+    fs.copyFile(path.join(rootPath, 'snap.manifest.json'), path.join(publicPath, 'snap.manifest.json')),
+  ]);
 }
+
+prepareArtifacts();
