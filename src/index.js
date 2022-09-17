f27d6e788417e457566d0688fb3a6f2c25eb1db1
Khadeeejah <wuraola81@gmail.com>
2022-09-17 11:04:50 +0400

    methods

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ const symbols = require('./symbols');
 const token = require('./token');
 const { getTokenPairSpotPrice } = require('./uniswap');
 
-async function lookupHandler(args) {
-  return await getTokenPairSpotPrice(args.tokenPair);
+async function lookupHandler(request) {
+  const consent = await wallet.request({
+    method: 'snap_confirm',
+    params: [
+      {
+        prompt: `Hello,!`,
+        description: 'This custom confirmation is just for display purposes.',
+        textAreaContent: `So you are trying to fetch spot prices for these token address`,
+      },
+    ],
+  });
+  if (!consent) {
+    return 'Sad to see you go 😭';
+  }
+  return getTokenPairSpotPrice(request.tokens);
 }
 
 async function identifyHandler({ erc, address }) {
